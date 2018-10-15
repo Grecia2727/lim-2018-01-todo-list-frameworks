3871e8bbe59698700eb98de4b4f3a387a87cba20
Grecia <gutierrezanicamalucero@gmail.com>
2018-10-15 16:36:28 -0500

    Updates

---
+++ precache-manifest.46c5e7f59dbc216627d1dfc07e13bca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40badda82c29eb5c80d5b9cf1c059f72",
+    "url": "/lim-2018-01-todo-list-frameworks/static/media/sandwhich-fondo-escritorio.40badda8.jpeg"
+  },
+  {
+    "revision": "50626c473dcd3c18512c",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/runtime~main.50626c47.js"
+  },
+  {
+    "revision": "e1c08618268a0df1e620",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/main.e1c08618.chunk.js"
+  },
+  {
+    "revision": "d377d35b8f5fe28b4d54",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/1.d377d35b.chunk.js"
+  },
+  {
+    "revision": "e1c08618268a0df1e620",
+    "url": "/lim-2018-01-todo-list-frameworks/static/css/main.3e1dfab3.chunk.css"
+  },
+  {
+    "revision": "88a03bc76e3a691258d50f14f420a8d1",
+    "url": "/lim-2018-01-todo-list-frameworks/index.html"
+  }
+];
